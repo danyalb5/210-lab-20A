915caad4f77d5008d028ba204feec117a72c91ef
danyalb5 <danyalbawani@gmail.com>
2024-10-16 22:48:08 -0700

    Updated constructors for random initialization.

---
+++ #include <iostream>.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+const int SIZE = 3;
+
+class Chair {
+private:
+    int legs;
+    double* prices;
+
+public:
+    // Updated constructors
+    Chair() {
+        srand(time(NULL)); // Seed random number generator
+        prices = new double[SIZE];
+        legs = rand() % 2 + 3; // Randomly 3 or 4 legs
+        for (int i = 0; i < SIZE; i++) {
+            int randomPrice = rand() % (99999 - 10000 + 1) + 10000; // Random price between 10000 and 99999 cents
+            prices[i] = randomPrice / 100.0; // Convert to dollars
+        }
+    }
+
+    Chair(int l, double p[]) {
+        prices = new double[SIZE];
+        legs = l;
+        for (int i = 0; i < SIZE; i++) {
+            prices[i] = p[i];
+        }
+    }
+
+    // Existing methods unchanged for this segment...
+};
